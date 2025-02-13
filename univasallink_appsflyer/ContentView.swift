61b96b016b9e918005abde8c2160e037bbb1d3d4
sayha <s-nakajima@pepabo.com>
2025-02-13 10:24:20 +0900

    Initial Commit

---
+++ univasallink_appsflyer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  univasallink_appsflyer
+//
+//  Created by s-nakajima on 2025/02/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ univasallink_appsflyer/univasallink_appsflyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  univasallink_appsflyerApp.swift
+//  univasallink_appsflyer
+//
+//  Created by s-nakajima on 2025/02/13.
+//
+
+import SwiftUI
+
+@main
+struct univasallink_appsflyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
